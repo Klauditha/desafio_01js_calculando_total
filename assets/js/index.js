511a88f4b475449588966e3d20db6ce6296699d6
Claudia Villarroel <claudia.villarroel@live.com>
2023-07-01 18:24:20 -0400

    WIP: Iniciando opciones en javascript

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+/* Inicializando variables*/
+let total=0;
+let cantidad=0;
+let precio=0;
+let color="";
+
+precio = document.querySelector('.valor_precio');
+total = document.querySelector('.total_resultado');
+cantidad = document.querySelector('.cantidad_resultado');
+color = document.querySelector('.color_resultado');
+total.innerHTML = 0;
+cantidad.innerHTML = 0;
